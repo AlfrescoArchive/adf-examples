d88572af5ef228d07544a8cd3ccc30fc0942d93b
Andras Popovics <popovics@ndras.hu>
2017-06-30 16:52:48 +0100

    1. Initial commit after ADF 1.5 app was created with yeoman generator

---
+++ ADF 1.6.1/Upgrade-from-1.5/app/app.module.ts
@@ -0,0 +1,92 @@
+/*!
+ * @license
+ * Copyright 2016 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Editor3DModule } from 'ng2-3d-editor';
+
+import { CoreModule } from 'ng2-alfresco-core';
+import { DataTableModule } from 'ng2-alfresco-datatable';
+
+import { SearchModule } from 'ng2-alfresco-search';
+import { DocumentListModule } from 'ng2-alfresco-documentlist';
+import { UploadModule } from 'ng2-alfresco-upload';
+
+import { TagModule } from 'ng2-alfresco-tag';
+import { ActivitiFormModule } from 'ng2-activiti-form';
+import { ActivitiTaskListModule } from 'ng2-activiti-tasklist';
+import { ActivitiProcessListModule } from 'ng2-activiti-processlist';
+import { AnalyticsModule } from 'ng2-activiti-analytics';
+import { LoginModule } from 'ng2-alfresco-login';
+import { UserInfoComponentModule } from 'ng2-alfresco-userinfo';
+import { ViewerModule } from 'ng2-alfresco-viewer';
+import { AppComponent } from './app.component';
+import { routing } from './app.routes';
+
+import {
+  HomeComponent,
+  SearchComponent,
+  SearchBarComponent,
+  ActivitiDemoComponent,
+  ActivitiAppsView,
+  FormViewer,
+  FormNodeViewer,
+  FilesComponent,
+  AboutComponent,
+  LoginDemoComponent,
+  SettingComponent
+} from './components/index';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        routing,
+        CoreModule.forRoot(),
+        DataTableModule.forRoot(),
+        SearchModule.forRoot(),
+        
+        DocumentListModule.forRoot(),
+        UploadModule.forRoot(),
+        ViewerModule.forRoot(),
+        ActivitiFormModule.forRoot(),
+        ActivitiTaskListModule.forRoot(),
+        ActivitiProcessListModule.forRoot(),
+        AnalyticsModule.forRoot(),
+        LoginModule.forRoot(),
+        UserInfoComponentModule.forRoot(),
+        Editor3DModule.forRoot(),
+        TagModule.forRoot()
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        SearchBarComponent,
+        SearchComponent,
+        ActivitiDemoComponent,
+        ActivitiAppsView,
+        FormViewer,
+        FormNodeViewer,
+        FilesComponent,
+        AboutComponent,
+        LoginDemoComponent,
+        SettingComponent
+    ],
+    providers: [],
+    bootstrap: [ AppComponent ]
+})
+export class AppModule { }
---
+++ ADF 1.6.1/Upgrade-from-1.5/app/components/about/about.component.ts
@@ -0,0 +1,60 @@
+/*!
+ * @license
+ * Copyright 2016 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { ObjectDataTableAdapter } from 'ng2-alfresco-datatable';
+import { LogService } from 'ng2-alfresco-core';
+
+@Component({
+    selector: 'about-page',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+    data: ObjectDataTableAdapter;
+
+    constructor(private http: Http,
+                private logService: LogService) {
+    }
+
+    ngOnInit() {
+        this.http.get('/versions.json').subscribe(response => {
+            let regexp = new RegExp('^(ng2-activiti|ng2-alfresco|alfresco-)');
+
+            let alfrescoPackages = Object.keys(response.json().dependencies).filter((val) => {
+                this.logService.log(val);
+                return regexp.test(val);
+            });
+
+            let alfrescoPackagesTableRappresentation = [];
+            alfrescoPackages.forEach((val) => {
+                this.logService.log(response.json().dependencies[val]);
+                alfrescoPackagesTableRappresentation.push({name: val, version: response.json().dependencies[val].version});
+            });
+
+            this.logService.log(alfrescoPackagesTableRappresentation);
+
+            this.data = new ObjectDataTableAdapter(alfrescoPackagesTableRappresentation, [
+                {type: 'text', key: 'name', title: 'Name', sortable: true},
+                {type: 'text', key: 'version', title: 'Version', sortable: true}
+            ]);
+        });
+    }
+}
