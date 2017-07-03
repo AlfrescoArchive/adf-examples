a4302879bc3e177282fc42b2cbfb3fad89f6ccd7
Eugenio Romano <eugenio.romano@alfresco.com>
2017-07-03 16:00:25 +0200

    rename 1.6.1

---
+++ ADF_1.6.1/Upgrade-from-1.5/app/app.routes.ts
@@ -0,0 +1,104 @@
+/*!
+ * @license
+ * Copyright 2016 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard, AuthGuardEcm, AuthGuardBpm } from 'ng2-alfresco-core';
+
+import {
+  AboutComponent,
+  HomeComponent,
+ SearchComponent,
+ FilesComponent,
+ ActivitiDemoComponent,
+  ActivitiAppsView,
+  FormViewer,
+  FormNodeViewer,
+  LoginDemoComponent,
+  SettingComponent
+} from './components/index';
+
+export const appRoutes: Routes = [
+  {path: 'login', component: LoginDemoComponent},
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [AuthGuard]
+  },
+  
+  {
+    path: 'files',
+    component: FilesComponent,
+    canActivate: [AuthGuardEcm]
+  },
+  {
+    path: 'files/:id',
+    component: FilesComponent,
+    canActivate: [AuthGuardEcm]
+  },
+  
+  
+  {
+    path: 'search',
+    component: SearchComponent,
+    canActivate: [AuthGuardEcm]
+  },
+  
+  
+  {
+    path: 'activiti',
+    component: ActivitiAppsView,
+    canActivate: [AuthGuardBpm]
+  },
+  {
+    path: 'activiti/apps',
+    component: ActivitiAppsView,
+    canActivate: [AuthGuardBpm]
+  },
+  {
+    path: 'activiti/apps/:appId/tasks',
+    component: ActivitiDemoComponent,
+    canActivate: [AuthGuardBpm]
+  },
+  {
+    path: 'activiti/appId/:appId',
+    component: ActivitiDemoComponent,
+    canActivate: [AuthGuardBpm]
+  },
+  {
+    path: 'activiti/tasks/:id',
+    component: FormViewer,
+    canActivate: [AuthGuardBpm]
+  },
+  {
+    path: 'activiti/tasksnode/:id',
+    component: FormNodeViewer,
+    canActivate: [AuthGuardBpm]
+  },
+  
+  {path: 'about', component: AboutComponent},
+  {path: 'settings', component: SettingComponent}
+
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
