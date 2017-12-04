6a6bfbccf5bf53259adad61e63ee5f2b0b4ba54e
VitoAlbano <vito.albano.123@gmail.com>
2017-12-04 17:05:12 +0000

    [ADF-2058] customised a search result list

--- ADF_2.0.0/search-customisation-example/src/app/search/search-demo.component.ts
+++ ADF_2.0.0/search-customisation-example/src/app/search/search-demo.component.ts
@@ -1,16 +1,36 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NotificationService } from '@alfresco/adf-core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { NotificationService, ThumbnailService } from '@alfresco/adf-core';
 import { SearchComponent } from '@alfresco/adf-content-services';
+import { MinimalNodeEntity } from 'alfresco-js-api';
 
 @Component({
   selector: 'app-custom-search',
   templateUrl: './search-demo.component.html',
-  styleUrls: ['./search-demo.component.css']
+  styleUrls: ['./search-demo.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class SearchDemoComponent implements OnInit {
+export class SearchDemoComponent {
 
   searchTerm = '';
 
-  ngOnInit() {
+  constructor(private thumbnailService: ThumbnailService) { }
+
+
+  getMimeTypeIcon(node: MinimalNodeEntity): string {
+    let mimeType;
+
+    if (node.entry.content && node.entry.content.mimeType) {
+      mimeType = node.entry.content.mimeType;
+    }
+    if (node.entry.isFolder) {
+      mimeType = 'folder';
+    }
+
+    return this.thumbnailService.getMimeTypeIcon(mimeType);
   }
+
+  onClick(item: MinimalNodeEntity) {
+     alert(' CLICKED ELEMENT ' + item.entry.name);
+  }
+
 }
