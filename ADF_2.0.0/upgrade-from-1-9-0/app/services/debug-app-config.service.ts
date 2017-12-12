99cca86ac89cd7833b9bb986f845ce8d620ba955
Eugenio Romano <eugenio.romano@alfresco.com>
2017-12-12 15:18:39 +0000

    10. Fix the debug-app-config.service

--- ADF_2.0.0/upgrade-from-1-9-0/app/services/debug-app-config.service.ts
+++ ADF_2.0.0/upgrade-from-1-9-0/app/services/debug-app-config.service.ts
@@ -1,20 +1,18 @@
-
-
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import { AppConfigService, StorageService } from '@alfresco/adf-core';
 
 @Injectable()
 export class DebugAppConfigService extends AppConfigService {
 
-    constructor(private storage: StorageService, http: Http) {
+    constructor(private storage: StorageService, http: HttpClient) {
         super(http);
     }
 
     /** @override */
     get<T>(key: string): T {
         if (key === 'ecmHost' || key === 'bpmHost') {
-            return <T>(<any>this.storage.getItem(key) || super.get<T>(key));
+            return <T> (<any> this.storage.getItem(key) || super.get<T>(key));
         }
         return super.get<T>(key);
     }
