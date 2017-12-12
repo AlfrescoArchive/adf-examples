6230f108c012830aef079cf53d1109f7efc65e24
Eugenio Romano <eugenio.romano@alfresco.com>
2017-12-12 15:32:05 +0000

    12. Fix the Search

--- ADF_2.0.0/upgrade-from-1-9-0/app/components/search/search-bar.component.ts
+++ ADF_2.0.0/upgrade-from-1-9-0/app/components/search/search-bar.component.ts
@@ -1,29 +1,32 @@
+/*!
+ * @license
+ * Copyright 2016 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { MinimalNodeEntity } from 'alfresco-js-api';
-import { AuthenticationService } from '@alfresco/adf-core';
 
 @Component({
     selector: 'search-bar',
-    templateUrl: './search-bar.component.html'
+    templateUrl: './search-bar.component.html',
+    styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent {
 
-    fileNodeId: string;
-    fileShowed: boolean = false;
-    searchTerm: string = '';
-
-    @Output()
-    expand = new EventEmitter();
-
-    constructor(public router: Router,
-                public authService: AuthenticationService) {
-    }
-
-    isLoggedIn(): boolean {
-        return this.authService.isLoggedIn();
+    constructor(public router: Router) {
     }
 
     /**
@@ -31,26 +34,19 @@ export class SearchBarComponent {
      *
      * @param event Parameters relating to the search
      */
-    onSearchSubmit(event) {
+    onSearchSubmit(event: KeyboardEvent) {
+        const value = (event.target as HTMLInputElement).value;
         this.router.navigate(['/search', {
-            q: event.value
+            q: value
         }]);
     }
 
     onItemClicked(event: MinimalNodeEntity) {
         if (event.entry.isFile) {
-            this.fileNodeId = event.entry.id;
-            this.fileShowed = true;
+            this.router.navigate(['/files', event.entry.id, 'view']);
         } else if (event.entry.isFolder) {
             this.router.navigate(['/files', event.entry.id]);
         }
     }
 
-    onSearchTermChange(event) {
-        this.searchTerm = event.value;
-    }
-
-    onExpandToggle(event) {
-        this.expand.emit(event);
-    }
 }
