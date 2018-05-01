b1c7a8e7df0ef0d34094d71930877f9370371fb7
Eugenio Romano <eugenio.romano@alfresco.com>
2018-05-01 16:16:22 +0100

    1. Generate your application

---
+++ ADF_2.3.0/logservice-adf/src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// ADF modules
+import { AdfModule } from './adf.module';
+
+// Custom stencils
+import { StencilsModule } from './stencils.module';
+
+// App components
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AppsComponent } from './apps/apps.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskDetailsComponent } from './task-details/task-details.component';
+import { DocumentlistComponent } from './documentlist/documentlist.component';
+import { StartProcessComponent } from './start-process/start-process.component';
+import { appRoutes } from './app.routes';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes // ,
+      // { enableTracing: true } // <-- debugging purposes only
+    ),
+
+    // ADF modules
+    AdfModule,
+
+    StencilsModule
+  ],
+  declarations: [
+    AppComponent,
+    AppsComponent,
+    HomeComponent,
+    LoginComponent,
+    TasksComponent,
+    TaskDetailsComponent,
+    DocumentlistComponent,
+    StartProcessComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ADF_2.3.0/logservice-adf/src/app/tasks/tasks.component.spec.ts
@@ -0,0 +1,33 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AdfModule } from '../adf.module';
+import { TasksComponent } from './tasks.component';
+
+describe('TasksComponent', () => {
+  let component: TasksComponent;
+  let fixture: ComponentFixture<TasksComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        BrowserAnimationsModule,
+        RouterTestingModule,
+        AdfModule
+      ],
+      declarations: [ TasksComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TasksComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
